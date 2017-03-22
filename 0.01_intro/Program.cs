1c440cf3b0edacc9f5d24465e94f7dd33994ed7d
dbpeetz <d.peetz516@gmail.com>
2017-03-22 10:13:20 -0400

    Working in the console intro

---
+++ 0.01_intro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace _0._01_intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+    }
+}
