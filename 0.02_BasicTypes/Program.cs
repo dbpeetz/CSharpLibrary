8ed691e8d18c71badfffb4ed11b41017b466d54a
dbpeetz <d.peetz516@gmail.com>
2017-03-22 12:13:53 -0400

    Basic Types and Strings Completed

---
+++ 0.02_BasicTypes/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._02_BasicTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Data Types");
+
+            //string name = "Devan";
+            //string teacher = "Paul";
+            int a;
+            a = 100;
+            string year = "1991";
+            bool t;
+            t = true;
+            bool f;
+            f = false;
+            float flt;
+            flt = 120f;
+            double number;
+            number = 12.35;
+            decimal dec;
+            dec = 0.1m;
+
+            Console.WriteLine("******Bronze******");
+            Console.WriteLine(t);
+            Console.WriteLine(f);
+            Console.WriteLine(flt);
+            Console.WriteLine(number);
+            Console.WriteLine(dec);
+            Console.WriteLine("******Silver******");
+            Console.WriteLine("The current year is " + a + "? " + f);
+            Console.WriteLine("********Gold************");
+            Convert.ToInt32(year);
+
+            //int numVal = Int32.Parse(year);
+            //Console.WriteLine(name);
+            Console.WriteLine("I was born in " + year);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ 0.03_strings/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._03_strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName;
+            string lastName;
+            string occupation;
+
+            firstName = "Devan";
+            lastName = "Peetz";
+            occupation = "Student";
+            string fullname = firstName + " " + lastName;
+            Console.WriteLine(fullname);
+
+            Console.WriteLine("Name: {0}" + "\n" + "Occupation: {1}", fullname, occupation);
+
+            //Silver Challenge
+            string first = "Use those types ";
+            string second = "to concatenate a string. ";
+            string third = "Any string will do";
+
+            Console.WriteLine("{0}" + "{1}" + "{2}", first, second, third);
+
+            //Bronze Challenge
+            string multiple = String.Format("0x{0:X} {0:E} {0:N}", Int64.MaxValue);
+
+            //Other silver challenge
+            string date = "3/22/2017";
+            Convert.ToDateTime(date);
+
+            Console.WriteLine("Dear Mr Jones, \nToday is " + DateTime.Now + ".  I'm enjoying class, " +
+                "and I think I like C# the best.\nI'd like to get a job as a developer after I finish this course.");
+
+            //Gold Challenge
+            string userName = "dpeetz";
+            string userName2 = "DPEETZ";
+
+            bool result = userName.Equals(userName2, StringComparison.Ordinal);
+
+            Console.WriteLine("Comparison: {0} and {1} are {2}", userName, userName2, result ? "equal." : "not equal.");
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
