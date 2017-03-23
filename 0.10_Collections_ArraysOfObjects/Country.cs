4d6850263e006650fee3413734ef475b064dc9a7
dbpeetz <d.peetz516@gmail.com>
2017-03-23 15:39:06 -0400

    Added Arrays of Objects

---
+++ 0.10_Collections_ArraysOfObjects/Country.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._10_Collections_ArraysOfObjects
+{
+    class Country
+    {
+
+        public Country(string name, string capital)
+        {
+            this.Name = name;
+            this.Capital = capital;
+        }
+
+        public string Name { get; set; }
+        public string Capital { get; set; }
+        
+        //Method
+
+        public void PrintBetterDetails()
+        {
+            Console.WriteLine("The capital of {0} is {1}.", this.Name, this.Capital);
+
+        }
+    }
+}
